c320ddffc9b30de641e245691c988deb154b13ce
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-02-01 10:21:19 -0300

    refatored code

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,43 +1,45 @@
 export const arrayEquals = (a, b) => {
-  return Array.isArray(a) &&
+  return (
+    Array.isArray(a) &&
     Array.isArray(b) &&
     a.length == b.length &&
     a.every((val, index) => val == b[index])
-}
-
-
+  );
+};
 
 export const verifyWinner = (tabuleiro) => {
-  //verifying rows 
+  //verifying rows
   for (let row of tabuleiro) {
-    if (row.every(val => val == 'x')) {
-      return 'x'
-    }
-    else if (row.every(val => val == 'o')) {
-      return 'o'
+    if (row.every((val) => val == "x")) {
+      return "x";
+    } else if (row.every((val) => val == "o")) {
+      return "o";
     }
   }
 
   //verifying column
-  if (tabuleiro.every((val, index) => val[index] == 'x')) {
-    return 'x'
+  if (tabuleiro.every((val, index) => val[index] == "x")) {
+    return "x";
   }
 
   //verifying column
-  else if (tabuleiro.every((val, index) => val[index] == 'o')) {
-    return 'o'
+  else if (tabuleiro.every((val, index) => val[index] == "o")) {
+    return "o";
   }
 
   //verifying diagonal
-  else if (tabuleiro.every((val, index) => val[val.length - (index + 1)] == 'o')) {
-    return 'o'
+  else if (
+    tabuleiro.every((val, index) => val[val.length - (index + 1)] == "o")
+  ) {
+    return "o";
   }
 
   //verifying diagonal
-  else if (tabuleiro.every((val, index) => val[val.length - (index + 1)] == 'x')) {
-    return 'x'
+  else if (
+    tabuleiro.every((val, index) => val[val.length - (index + 1)] == "x")
+  ) {
+    return "x";
   }
 
-  return false
-}
-
+  return false;
+};
